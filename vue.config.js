b1bf5b88840fc0ae5277af8b5c92fabeb3deb1eb
Efimov Roman <33133251+efimromulon@users.noreply.github.com>
2019-04-09 18:49:55 +0300

    preloader circles

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,6 @@ module.exports = {
 				data: `@import '@/sass/buttons/_theme-white-buttons.sass';@import '@/sass/themes/_theme-white.sass';@import '@/sass/abstracts/_mixins.sass';@import '@/sass/abstracts/_variables.sass';@import '@/sass/abstracts/_variables.sass';@import '@/sass/abstracts/_placeholders.sass';@import '@/sass/base/_reset.sass'; @import '@/sass/base/_typography.sass';@import '@/sass/custom-leaflet/_leaflet.sass';`
 			}
 		}
-	}
+	},
+	publicPath: process.env.NODE_ENV === 'production' ? 'http://hana103:8051/sngias/oilix_prd/map_dev/' : '/'
 };
